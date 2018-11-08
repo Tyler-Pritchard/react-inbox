80df4d6b0f24133d88f5823eaf57d1f4b913fa3f
Tyler Pritchard <pritchard.tyler@gmail.com>
2018-11-08 11:42:51 -0800

    added temlate info

---
+++ src/components/MessageList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+//builds a message list from Message
+//can be class component instead of functional-- personal preference
+//will import message component later
+import Message from './Message'
+
+class MessageList extends Component {
+    render() {
+    //use "Message: With Labels" from Inbox Style Guide because it has the most on it
+
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default MessageList
---
+++ src/components/Toolbar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+//not sure if stateful or not, but doesn't matter because we don't use state inside of them
+
+class Toolbar extends Component {
+    render() {
+        return (
+        <div>
+            
+        </div>
+        )
+    }
+}
+export default Toobar
